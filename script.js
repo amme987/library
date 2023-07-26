0ecc3b563a3d3ee50aea8b7671ecc4bac2030790
amme987 <emmagination97@gmail.com>
2023-07-26 16:34:33 -0400

    Display books added to library

--- script.js
+++ script.js
@@ -22,6 +22,31 @@ Book.prototype.info = function () {
 };
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
-console.log(theHobbit.info());
+const rhythmOfWar = new Book("Rhythm of War", "Brandon Sanderson", 1232, false);
+
+// Test adding books to library
+myLibrary.push(theHobbit);
+myLibrary.push(rhythmOfWar);
+
+// for (let books in myLibrary) {
+//   for (let keys in Object.keys(myLibrary[books])) {
+//     console.log(Object.values(myLibrary[books])[keys]);
+//   }
+// }
 
 function addBookToLibrary() {}
+
+function displayBooks() {
+  const table = document.querySelector("table");
+  for (let books in myLibrary) {
+    const row = document.createElement("tr");
+    table.appendChild(row);
+    for (let keys in Object.keys(myLibrary[books])) {
+      const data = document.createElement("td");
+
+      row.appendChild(data).textContent = Object.values(myLibrary[books])[keys];
+    }
+  }
+}
+
+displayBooks();
